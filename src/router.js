2b38affcca451e062767fd0a21b9c07500d107fa
Vivian Lo <yunyubee@gmail.com>
2019-11-14 13:32:16 +0800

    Add router of /restaurants, /restaurants/feeds, /restaurants/top, /users/top

--- src/router.js
+++ src/router.js
@@ -3,12 +3,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import NotFound from './views/NotFound.vue'
 import SignIn from './views/SignIn.vue'
+import Restaurants from './views/Restaurants.vue'
 
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'root',
+      redirect: '/restaurants'
+    },
     {
       path: '/signin',
       name: 'sign-in',
@@ -18,6 +24,26 @@ export default new Router({
       path: '/signup',
       name: 'sign-up',
       component: () => import('./views/SignUp.vue')
+    },
+    {
+      path: '/restaurants',
+      name: 'restaurants',
+      component: Restaurants
+    },
+    {
+      path: '/restaurants/feeds',
+      name: 'restaurants-feeds',
+      component: () => import('./views/RestaurantsFeeds.vue')
+    },
+    {
+      path: '/restaurants/top',
+      name: 'restaurants-tops',
+      component: () => import('./views/RestaurantsTop.vue')
+    },
+    {
+      path: '/users/top',
+      name: 'users-top',
+      component: () => import('./views/UsersTop.vue')
     },
     {
       path: '*',
