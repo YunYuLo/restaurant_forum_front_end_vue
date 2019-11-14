4307ab4847aa954dc3df50ea67f1f99dc4f02ce1
Vivian Lo <yunyubee@gmail.com>
2019-11-14 13:16:12 +0800

    Add navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <!-- ./src/App.vue -->
 <template>
   <div id="restaurant-forum">
-    <router-view />
+    <Navbar/>
+    <main
+      role="main"
+      class="mt-5 bg-white"
+    >
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
 export default {
+  components:{
+    Navbar
+  },
   name: 'App'
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import NotFound from './views/NotFound.vue'
 import SignIn from './views/SignIn.vue'
 
+
 Vue.use(Router)
 
 export default new Router({
@@ -12,6 +13,11 @@ export default new Router({
       path: '/signin',
       name: 'sign-in',
       component: SignIn
+    },
+    {
+      path: '/signup',
+      name: 'sign-up',
+      component: () => import('./views/SignUp.vue')
     },
     {
       path: '*',
