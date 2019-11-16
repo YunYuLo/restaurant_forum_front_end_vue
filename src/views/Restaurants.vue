6a4cb9b3e391ac43e68e777ce51ffb7e68cd92f4
Vivian Lo <yunyubee@gmail.com>
2019-11-16 15:23:35 +0800

    add /restaurants RestaurantsPagination component

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -13,7 +13,13 @@
       />
     </div>
 
-    <!-- 分頁標籤 RestaurantPagination -->
+    <!-- 分頁標籤 RestaurantsPagination -->
+    <RestaurantsPagination
+     v-if="totalPage > 1"
+     :category-id="categoryId"
+     :current-page="currentPage"
+     :total-page="totalPage"
+     />
   </div>
 </template>
 
@@ -21,6 +27,8 @@
 import NavTabs from './../components/NavTabs'
 import RestaurantCard from './../components/RestaurantCard'
 import RestaurantsNavPills from './../components/RestaurantsNavPills'
+import RestaurantsPagination from './../components/RestaurantsPagination'
+
 
 const dummyData = {
   restaurants: [
@@ -295,7 +303,8 @@ export default {
   components: {
     NavTabs,
     RestaurantCard,
-    RestaurantsNavPills
+    RestaurantsNavPills,
+    RestaurantsPagination
   },
   data () {
     return {
