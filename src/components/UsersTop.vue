62921b2cecd6a149ec668b0f5c368ce317cd8cf9
Vivian Lo <yunyubee@gmail.com>
2019-11-16 16:42:07 +0800

    add UsersTop component

---
+++ src/components/UsersTop.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="col-3">
+    <a href="#">
+      <img
+        :src="user.image"
+        width="140px"
+        height="140px"
+      >
+    </a>
+    <h2>{{user.name}}</h2>
+    <span class="badge badge-secondary">
+    追蹤人數：{{user.FollowerCount}}</span>
+    <p class="mt-3">
+      <button
+        v-if="user.isFollowed"
+        type="button"
+        class="btn btn-danger"
+        @click.stop.prevent="unfollowed"
+      >
+        取消追蹤
+      </button>
+      <button
+        v-else
+        type="button"
+        class="btn btn-primary"
+        @click.stop.prevent="followed"
+      >
+        追蹤
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    initialUser:{
+      type: Object,
+      required: true
+    }
+  },
+  data(){
+    return {
+      user: this.initialUser
+    }
+  },
+  methods:{
+    followed(){
+      this.user={
+        ...this.user,
+        isFollowed:true,
+        FollowerCount: this.user.FollowerCount+1
+      }
+    },
+    unfollowed(){
+      this.user={
+        ...this.user,
+        isFollowed:false,
+        FollowerCount: this.user.FollowerCount-1
+      }
+    }
+  }
+}
+</script>
