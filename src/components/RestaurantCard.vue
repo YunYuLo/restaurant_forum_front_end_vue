a29eaee4527f79cc41a66bb840ecd3991f30eebe
Vivian Lo <yunyubee@gmail.com>
2019-11-22 22:22:32 +0800

    API integration of RestaurantsCard page-add/deleteFavorite

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -7,58 +7,46 @@
         alt="Card image cap"
         width="286px"
         height="180px"
-      >
+      />
       <div class="card-body">
         <p class="card-text title-wrap">
-          <router-link 
-            :to="{name:'restaurant', params:{id:restaurant.id}}"
-          >
-            {{restaurant.name}}
-          </router-link>
+          <router-link :to="{name:'restaurant', params:{id:restaurant.id}}">{{restaurant.name}}</router-link>
         </p>
         <span class="badge badge-secondary">{{restaurant.Category.name}}</span>
-        <p class="card-text text-truncate">
-          {{restaurant.description}}
-        </p>
+        <p class="card-text text-truncate">{{restaurant.description}}</p>
       </div>
       <div class="card-footer">
         <button
           v-if="restaurant.isFavorited"
           type="button"
           class="btn btn-danger btn-border favorite mr-2"
-          @click.stop.prevent = "deleteFavorite"
-        >
-          移除最愛
-        </button>
+          @click.stop.prevent="deleteFavorite(restaurant.id)"
+        >移除最愛</button>
         <button
           v-else
           type="button"
           class="btn btn-primary btn-border favorite mr-2"
-          @click.stop.prevent = "addFavorite"
-        >
-          加到最愛
-        </button>
+          @click.stop.prevent="addFavorite(restaurant.id)"
+        >加到最愛</button>
         <button
           v-if="restaurant.isLiked"
           type="button"
           class="btn btn-danger like mr-2"
-          @click.stop.prevent = "deleteLike"
-        >
-          Unlike
-        </button>
+          @click.stop.prevent="deleteLike"
+        >Unlike</button>
         <button
           v-else
           type="button"
           class="btn btn-primary like mr-2"
-          @click.stop.prevent = "addLike"
-        >
-          Like
-        </button>
+          @click.stop.prevent="addLike"
+        >Like</button>
       </div>
     </div>
   </div>
 </template>
 <script>
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
 export default {
   props: {
     initialRestaurant: {
@@ -66,36 +54,62 @@ export default {
       required: true
     }
   },
-  data () {
+  data() {
     return {
       restaurant: this.initialRestaurant
-    }
+    };
   },
-  methods:{
-    addFavorite(){
-      this.restaurant={
-        ...this.restaurant,
-        isFavorited:true
+  methods: {
+    async addFavorite(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.addFavorite({
+          restaurantId
+        });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: true
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳加入最愛，請稍後再試"
+        });
       }
     },
-    deleteFavorite(){
-      this.restaurant={
-        ...this.restaurant,
-        isFavorited:false
+    async deleteFavorite(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteFavorite({
+          restaurantId
+        });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: false
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳從最愛移除，請稍後再試"
+        });
       }
     },
-    addLike(){
-      this.restaurant={
+    addLike() {
+      this.restaurant = {
         ...this.restaurant,
-        isLiked:true
-      }
+        isLiked: true
+      };
     },
-    deleteLike(){
-      this.restaurant={
+    deleteLike() {
+      this.restaurant = {
         ...this.restaurant,
-        isLiked:false
-      }
+        isLiked: false
+      };
     }
   }
-}
+};
 </script>
