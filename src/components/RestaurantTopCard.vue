cc88f2ab063c357d2e9a8f1398afd7c6ae8e5f6f
Vivian Lo <yunyubee@gmail.com>
2019-11-23 01:54:34 +0800

    API integration of RestaurantsTop page

--- src/components/RestaurantTopCard.vue
+++ src/components/RestaurantTopCard.vue
@@ -20,13 +20,13 @@
             v-if="restaurant.isFavorited"
             type="button"
             class="btn btn-danger mr-2"
-            @click.stop.prevent="deleteFavorite"
+            @click.stop.prevent="deleteFavorite(restaurant.id)"
           >移除最愛</button>
           <button
             v-else
             type="button"
             class="btn btn-primary"
-            @click.stop.prevent="addFavorite"
+            @click.stop.prevent="addFavorite(restaurant.id)"
           >加到最愛</button>
         </div>
       </div>
@@ -34,6 +34,9 @@
   </div>
 </template>
 <script>
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
+
 export default {
   props: {
     initialRestaurant: {
@@ -47,17 +50,43 @@ export default {
     };
   },
   methods: {
-    addFavorite() {
-      this.restaurant = {
-        ...this.restaurant,
-        isFavorited: true
-      };
+    async addFavorite(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.addFavorite({
+          restaurantId
+        });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: true
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳加入最愛，請稍後再試"
+        });
+      }
     },
-    deleteFavorite() {
-      this.restaurant = {
-        ...this.restaurant,
-        isFavorited: false
-      };
+    async deleteFavorite(restaurantId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteFavorite({
+          restaurantId
+        });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: false
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法將餐廳從最愛移除，請稍後再試"
+        });
+      }
     }
   }
 };
