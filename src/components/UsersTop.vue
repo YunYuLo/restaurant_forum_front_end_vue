50e7bf911e300beaff7828b4abd8a35b5b011f43
Vivian Lo <yunyubee@gmail.com>
2019-11-23 01:35:03 +0800

    API integration of UsersTop page

--- src/components/UsersTop.vue
+++ src/components/UsersTop.vue
@@ -1,64 +1,80 @@
 <template>
   <div class="col-3">
-    <a href="#">
-      <img
-        :src="user.image"
-        width="140px"
-        height="140px"
-      >
-    </a>
+    <router-link :to="{name: 'user', params: {id: user.id}}">
+      <img :src="user.image" width="140px" height="140px" />
+    </router-link>
     <h2>{{user.name}}</h2>
-    <span class="badge badge-secondary">
-    追蹤人數：{{user.FollowerCount}}</span>
+    <span class="badge badge-secondary">追蹤人數：{{user.FollowerCount}}</span>
     <p class="mt-3">
       <button
         v-if="user.isFollowed"
         type="button"
         class="btn btn-danger"
-        @click.stop.prevent="unfollowed"
-      >
-        取消追蹤
-      </button>
+        @click.stop.prevent="removeFollowing(user.id)"
+      >取消追蹤</button>
       <button
         v-else
         type="button"
         class="btn btn-primary"
-        @click.stop.prevent="followed"
-      >
-        追蹤
-      </button>
+        @click.stop.prevent="addFollowing(user.id)"
+      >追蹤</button>
     </p>
   </div>
 </template>
 
 <script>
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
+
 export default {
-  props:{
-    initialUser:{
+  props: {
+    initialUser: {
       type: Object,
       required: true
     }
   },
-  data(){
+  data() {
     return {
       user: this.initialUser
-    }
+    };
   },
-  methods:{
-    followed(){
-      this.user={
-        ...this.user,
-        isFollowed:true,
-        FollowerCount: this.user.FollowerCount+1
+  methods: {
+    async addFollowing(userId) {
+      try {
+        const { data, statusText } = await usersAPI.addFollowing({ userId });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        this.user = {
+          ...this.user,
+          isFollowed: true,
+          FollowerCount: this.user.FollowerCount + 1
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法加入追蹤，請稍後再試"
+        });
       }
     },
-    unfollowed(){
-      this.user={
-        ...this.user,
-        isFollowed:false,
-        FollowerCount: this.user.FollowerCount-1
+    async removeFollowing(userId) {
+      try {
+        const { data, statusText } = await usersAPI.deleteFollowing({ userId });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        this.user = {
+          ...this.user,
+          isFollowed: false,
+          FollowerCount: this.user.FollowerCount - 1
+        };
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法移除追蹤，請稍後再試"
+        });
       }
     }
   }
-}
+};
 </script>
--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -1,309 +1,49 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <h1 class="mt-5">
-      美食達人
-    </h1>
-    <hr>
+    <h1 class="mt-5">美食達人</h1>
+    <hr />
     <div class="row text-center">
-    <!-- UsersTop -->
-    <UsersTop
-      v-for="user in users"
-      :key="user.id"
-      :initial-user="user"
-      />
+      <UsersTop v-for="user in users" :key="user.id" :initial-user="user" />
     </div>
   </div>
 </template>
 <script>
-import NavTabs from './../components/NavTabs'
-import UsersTop from './../components/UsersTop'
+import NavTabs from "./../components/NavTabs";
+import UsersTop from "./../components/UsersTop";
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
 
-const dummyData = {
-    "users": [
-        {
-            "id": 1,
-            "name": "root test",
-            "email": "root@example.com",
-            "password": "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-            "isAdmin": true,
-            "image": "https://i.imgur.com/58ImzMM.png",
-            "createdAt": "2019-07-30T16:24:54.983Z",
-            "updatedAt": "2019-09-15T07:49:43.737Z",
-            "Followers": [
-                {
-                    "id": 12,
-                    "name": "eason",
-                    "email": "eason@example.com",
-                    "password": "$2a$10$vY3NIexoMkSUI.4ph6Y8teakUPKo7jN1lWhKkJooyVeGMEiNc7CyW",
-                    "isAdmin": null,
-                    "image": "https://i.imgur.com/nbHsOOs.jpg",
-                    "createdAt": "2019-11-14T05:50:54.996Z",
-                    "updatedAt": "2019-11-14T05:52:02.830Z",
-                    "Followship": {
-                        "followerId": 12,
-                        "followingId": 1,
-                        "createdAt": "2019-11-14T05:51:27.831Z",
-                        "updatedAt": "2019-11-14T05:51:27.831Z"
-                    }
-                },
-                {
-                    "id": 1,
-                    "name": "root test",
-                    "email": "root@example.com",
-                    "password": "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-                    "isAdmin": true,
-                    "image": "https://i.imgur.com/58ImzMM.png",
-                    "createdAt": "2019-07-30T16:24:54.983Z",
-                    "updatedAt": "2019-09-15T07:49:43.737Z",
-                    "Followship": {
-                        "followerId": 1,
-                        "followingId": 1,
-                        "createdAt": "2019-09-16T03:56:36.845Z",
-                        "updatedAt": "2019-09-16T03:56:36.845Z"
-                    }
-                },
-                {
-                    "id": 2,
-                    "name": "user1",
-                    "email": "user1@example.com",
-                    "password": "$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y",
-                    "isAdmin": false,
-                    "image": "https://i.imgur.com/Q14p2le.jpg",
-                    "createdAt": "2019-07-30T16:24:55.204Z",
-                    "updatedAt": "2019-09-09T05:34:35.986Z",
-                    "Followship": {
-                        "followerId": 2,
-                        "followingId": 1,
-                        "createdAt": "2019-08-01T10:27:45.616Z",
-                        "updatedAt": "2019-08-01T10:27:45.616Z"
-                    }
-                }
-            ],
-            "FollowerCount": 3,
-            "isFollowed": true
-        },
-        {
-            "id": 3,
-            "name": "user2",
-            "email": "user2@example.com",
-            "password": "$2a$10$tsvcnSFsJvdvs2NLm9rW.uYbah93Xl5cTYcQnSeK3sjEopj.NGzk2",
-            "isAdmin": false,
-            "image": "https://i.imgur.com/OezkRwO.jpg",
-            "createdAt": "2019-07-30T16:24:55.422Z",
-            "updatedAt": "2019-09-15T09:14:03.454Z",
-            "Followers": [
-                {
-                    "id": 1,
-                    "name": "root test",
-                    "email": "root@example.com",
-                    "password": "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-                    "isAdmin": true,
-                    "image": "https://i.imgur.com/58ImzMM.png",
-                    "createdAt": "2019-07-30T16:24:54.983Z",
-                    "updatedAt": "2019-09-15T07:49:43.737Z",
-                    "Followship": {
-                        "followerId": 1,
-                        "followingId": 3,
-                        "createdAt": "2019-09-16T03:57:51.814Z",
-                        "updatedAt": "2019-09-16T03:57:51.814Z"
-                    }
-                },
-                {
-                    "id": 2,
-                    "name": "user1",
-                    "email": "user1@example.com",
-                    "password": "$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y",
-                    "isAdmin": false,
-                    "image": "https://i.imgur.com/Q14p2le.jpg",
-                    "createdAt": "2019-07-30T16:24:55.204Z",
-                    "updatedAt": "2019-09-09T05:34:35.986Z",
-                    "Followship": {
-                        "followerId": 2,
-                        "followingId": 3,
-                        "createdAt": "2019-08-01T10:27:49.094Z",
-                        "updatedAt": "2019-08-01T10:27:49.094Z"
-                    }
-                }
-            ],
-            "FollowerCount": 2,
-            "isFollowed": true
-        },
-        {
-            "id": 9,
-            "name": "mos",
-            "email": "user6@example.com",
-            "password": "$2a$10$5mMHAYpCwz6gF0NPnZZdA.h8jbH2zIfvhErYNTZ9DXcss4G6ifmv2",
-            "isAdmin": null,
-            "image": null,
-            "createdAt": "2019-09-15T11:44:58.101Z",
-            "updatedAt": "2019-09-15T11:44:58.101Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 2,
-            "name": "user1",
-            "email": "user1@example.com",
-            "password": "$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y",
-            "isAdmin": false,
-            "image": "https://i.imgur.com/Q14p2le.jpg",
-            "createdAt": "2019-07-30T16:24:55.204Z",
-            "updatedAt": "2019-09-09T05:34:35.986Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 6,
-            "name": "cjl19670@aklqo.com",
-            "email": "cjl19670@aklqo.com",
-            "password": "$2a$10$mlS72JE7IffnhMTp7HGyfORzvpVrl.0zUNWxveHWo8/OiA.Stl8Ne",
-            "isAdmin": null,
-            "image": null,
-            "createdAt": "2019-08-15T09:10:17.401Z",
-            "updatedAt": "2019-08-15T09:10:17.401Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 8,
-            "name": "mark",
-            "email": "mark@gmail.com",
-            "password": "$2a$10$uegR/2XVI7S6UIQLlZxg6uQh4rdHZrTINyH7dmjApFueGg6A0LVZO",
-            "isAdmin": null,
-            "image": null,
-            "createdAt": "2019-09-08T05:54:22.376Z",
-            "updatedAt": "2019-09-08T05:54:22.376Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 5,
-            "name": "123",
-            "email": "123+v1@gmail.com",
-            "password": "$2a$10$aFObNZTWYpkh86oGEz647OCCg27GkIEqOBZUzBHttZavLtw2PosJO",
-            "isAdmin": false,
-            "image": null,
-            "createdAt": "2019-08-13T05:01:17.528Z",
-            "updatedAt": "2019-09-15T09:14:14.189Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 7,
-            "name": "買東西",
-            "email": "sun@gmail.com",
-            "password": "$2a$10$PyLZbH.VtrtJbxiteKbKbORiUyI83QF0sqdNCD3ntQh5dq1Nt6N/m",
-            "isAdmin": null,
-            "image": null,
-            "createdAt": "2019-09-08T05:52:45.681Z",
-            "updatedAt": "2019-09-08T05:52:45.681Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 10,
-            "name": "bb",
-            "email": "bb@example.com",
-            "password": "$2a$10$UHkAELuG7LYywkEfWD83POeZsgSaVfKHedxHmwS9YuBgMETMx8Kcy",
-            "isAdmin": null,
-            "image": null,
-            "createdAt": "2019-09-26T10:31:54.665Z",
-            "updatedAt": "2019-09-26T10:31:54.665Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 4,
-            "name": "123",
-            "email": "123@gmail.com",
-            "password": "$2a$10$3DgQkLtWS7ud3ewuWdmRuOiFhRsgcE1SzOYAzo4/wV.rOWP3rNcRC",
-            "isAdmin": false,
-            "image": null,
-            "createdAt": "2019-08-13T05:00:15.406Z",
-            "updatedAt": "2019-09-30T11:49:25.356Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 11,
-            "name": "parag",
-            "email": "p@mail.com",
-            "password": "$2a$10$a6Ce2r46Xd1iFF4EyXJu3uyCQE3jpEq2uy0ex.qG3ihcXMxmL0Tyu",
-            "isAdmin": null,
-            "image": null,
-            "createdAt": "2019-10-07T09:03:32.408Z",
-            "updatedAt": "2019-10-07T09:03:32.408Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 12,
-            "name": "eason",
-            "email": "eason@example.com",
-            "password": "$2a$10$vY3NIexoMkSUI.4ph6Y8teakUPKo7jN1lWhKkJooyVeGMEiNc7CyW",
-            "isAdmin": null,
-            "image": "https://i.imgur.com/nbHsOOs.jpg",
-            "createdAt": "2019-11-14T05:50:54.996Z",
-            "updatedAt": "2019-11-14T05:52:02.830Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 13,
-            "name": "lib1",
-            "email": "lib1@example.com",
-            "password": "$2a$10$9Nvavjenmx.mdlrTFFB6ZeNzG7UACZ2oFC.2Xbr5/yVE2WJa0g9MS",
-            "isAdmin": null,
-            "image": null,
-            "createdAt": "2019-11-15T16:38:45.158Z",
-            "updatedAt": "2019-11-15T16:38:45.158Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        },
-        {
-            "id": 14,
-            "name": "lib2",
-            "email": "lib2@example.com",
-            "password": "$2a$10$8lGwKR9SwRrTR4V3I5rZo.kU5Mfq90Gh4eBVlSzt/RRC6IdzOR3A2",
-            "isAdmin": null,
-            "image": null,
-            "createdAt": "2019-11-15T16:40:44.274Z",
-            "updatedAt": "2019-11-15T16:40:44.274Z",
-            "Followers": [],
-            "FollowerCount": 0,
-            "isFollowed": false
-        }
-    ]
-}
 export default {
   components: {
     NavTabs,
     UsersTop
   },
-  data(){
-    return{
-      users:[]
-    }
+  data() {
+    return {
+      users: []
+    };
   },
-  created(){
-    this.fetchUsers()
+  created() {
+    this.fetchTopUsers();
   },
-  methods:{
-    fetchUsers(){
-      this.users = dummyData.users
+  methods: {
+    async fetchTopUsers() {
+      try {
+        const { data, statusText } = await usersAPI.getTopUsers();
+
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+
+        this.users = data.users;
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法取得美食達人，請稍後再試"
+        });
+      }
     }
   }
-}
+};
 </script>
