1e37a933b7a2ac73bc9a4f606179cf255ed13f32
Vivian Lo <yunyubee@gmail.com>
2019-11-24 17:34:21 +0800

    API integration of Restaurant page and RestaurantDetail component

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -2,152 +2,99 @@
   <div class="container py-5">
     <h1>餐廳描述頁</h1>
     <!-- 餐廳資訊頁 RestaurantDetail -->
-    <RestaurantDetail
-      :initial-restaurant="restaurant"/>
-    <hr>
+    <RestaurantDetail :initial-restaurant="restaurant" />
+    <hr />
     <!-- 餐廳評論 RestaurantComments -->
-    <RestaurantComments :restaurant-comments="restaurantComments" 
-    @after-delete-comment="afterDeleteComment"
+    <RestaurantComments
+      :restaurant-comments="restaurantComments"
+      @after-delete-comment="afterDeleteComment"
     />
     <!-- 新增評論 CreateComment -->
-    <CreateComment 
-     :restaurant-id="restaurant.id"
-     @after-create-comment="afterCreateComment"
-     />
+    <CreateComment :restaurant-id="restaurant.id" @after-create-comment="afterCreateComment" />
   </div>
 </template>
 <script>
 /* eslint-disable */
-import RestaurantDetail from './../components/RestaurantDetail'
-import RestaurantComments from './../components/RestaurantComments'
-import CreateComment from './../components/CreateComment'
+import RestaurantDetail from "./../components/RestaurantDetail";
+import RestaurantComments from "./../components/RestaurantComments";
+import CreateComment from "./../components/CreateComment";
+import restaurantsAPI from "./../apis/restaurants";
+import { Toast } from "./../utils/helpers";
+import { mapState } from "vuex";
 
-
-
-const dummyData = {
-  currentUser: {
-    id: 1,
-    name: '管理者',
-    email: 'root@example.com',
-    image: 'https://i.pravatar.cc/300',
-    isAdmin: true
-  },
-  isAuthenticated: true,
-  "restaurant": {
-        "id": 1,
-        "name": "Laurence Reynolds",
-        "tel": "1-657-067-3756 x9782",
-        "address": "187 Kirlin Squares",
-        "opening_hours": "08:00",
-        "description": "sit est mollitia",
-        "image": "https://loremflickr.com/320/240/restaurant,food/?random=91.29816290184887",
-        "viewCounts": 65,
-        "createdAt": "2019-07-30T16:24:55.432Z",
-        "updatedAt": "2019-11-16T09:55:32.077Z",
-        "CategoryId": 3,
-        "Category": {
-            "id": 3,
-            "name": "義大利料理",
-            "createdAt": "2019-07-30T16:24:55.429Z",
-            "updatedAt": "2019-07-30T16:24:55.429Z"
-        },
-        "FavoritedUsers": [],
-        "LikedUsers": [
-            {
-                "id": 1,
-                "name": "root test",
-                "email": "root@example.com",
-                "password": "$2a$10$OJ3jR93XlEMrQtYMWOIQh.EINWgaRFTXkd0Xi5OC/Vz4maztUXEPe",
-                "isAdmin": true,
-                "image": "https://i.imgur.com/58ImzMM.png",
-                "createdAt": "2019-07-30T16:24:54.983Z",
-                "updatedAt": "2019-09-15T07:49:43.737Z",
-                "Like": {
-                    "UserId": 1,
-                    "RestaurantId": 1,
-                    "createdAt": "2019-07-30T16:25:58.629Z",
-                    "updatedAt": "2019-07-30T16:25:58.629Z"
-                }
-            }
-        ],
-        "Comments": [
-            {
-                "id": 1,
-                "text": "Sed dolores labore.",
-                "UserId": 2,
-                "RestaurantId": 1,
-                "createdAt": "2019-07-30T16:24:55.451Z",
-                "updatedAt": "2019-07-30T16:24:55.451Z",
-                "User": {
-                    "id": 2,
-                    "name": "user1",
-                    "email": "user1@example.com",
-                    "password": "$2a$10$oNyp9cr8jG7NulbUr56g6e3yvwnttFkoBAmtUYAeQuXkcdFz0Ko6y",
-                    "isAdmin": false,
-                    "image": "https://i.imgur.com/Q14p2le.jpg",
-                    "createdAt": "2019-07-30T16:24:55.204Z",
-                    "updatedAt": "2019-09-09T05:34:35.986Z"
-                }
-            }
-        ]
-    },
-    "isFavorited": false,
-    "isLiked": true,
-}
 export default {
-  components:{
+  components: {
     RestaurantDetail,
     RestaurantComments,
     CreateComment
   },
-  data () {
+  data() {
     return {
       restaurant: {
         id: undefined,
-        name: '',
-        categoryName: '',
-        image: '',
-        openingHours: '',
-        tel: '',
-        address: '',
-        description: '',
+        name: "",
+        categoryName: "",
+        image: "",
+        openingHours: "",
+        tel: "",
+        address: "",
+        description: "",
         isFavorited: false,
-        isLiked: false,
+        isLiked: false
       },
-      restaurantComments: [],
-      currentUser: dummyData.currentUser
-    }
+      restaurantComments: []
+    };
   },
-  created () {
-    const { id: restaurantId } = this.$route.params
-    this.fetchRestaurant(restaurantId)
+  created() {
+    const { id: restaurantId } = this.$route.params;
+    this.fetchRestaurant(restaurantId);
+  },
+  beforeRouteUpdate(to, from, next) {
+    const { id: restaurantId } = to.params;
+    this.fetchRestaurant(restaurantId);
+    next();
+  },
+  //從 Vuex 取得 currentUser 的資料
+  computed: {
+    ...mapState(["currentUser"])
   },
   methods: {
-    fetchRestaurant (restaurantId) {
-      console.log('fetchRestaurant id: ', restaurantId)
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data, statusText } = await restaurantsAPI.getRestaurant({
+          restaurantId
+        });
+        if (statusText !== "OK") {
+          throw new Error(statusText);
+        }
+        this.restaurant = {
+          id: data.restaurant.id,
+          name: data.restaurant.name,
+          categoryName: data.restaurant.Category.name,
+          image: data.restaurant.image,
+          openingHours: data.restaurant.opening_hours,
+          tel: data.restaurant.tel,
+          address: data.restaurant.address,
+          description: data.restaurant.description,
+          isFavorited: data.isFavorited,
+          isLiked: data.isLiked
+        };
 
-      this.restaurant = {
-        id: dummyData.restaurant.id,
-        name: dummyData.restaurant.name,
-        categoryName: dummyData.restaurant.Category.name,
-        image: dummyData.restaurant.image,
-        openingHours: dummyData.restaurant.opening_hours,
-        tel: dummyData.restaurant.tel,
-        address: dummyData.restaurant.address,
-        description: dummyData.restaurant.description,
-        isFavorited: dummyData.isFavorited,
-        isLiked: dummyData.isLiked,
+        this.restaurantComments = data.restaurant.Comments;
+      } catch (error) {
+        Toast.fire({
+          type: "error",
+          title: "無法取得餐廳資料，請稍後再試"
+        });
       }
-
-      this.restaurantComments = dummyData.restaurant.Comments
     },
-    afterDeleteComment (commentId){
+    afterDeleteComment(commentId) {
       this.restaurantComments = this.restaurantComments.filter(
-        comment=>comment.id!==commentId
-      )
+        comment => comment.id !== commentId
+      );
     },
-    afterCreateComment (payload){
-      const {commentId, restaurantId, text}=payload
+    afterCreateComment(payload) {
+      const { commentId, restaurantId, text } = payload;
       this.restaurantComments.push({
         id: commentId,
         RestaurantId: restaurantId,
@@ -157,8 +104,8 @@ export default {
         },
         text,
         createdAt: new Date()
-      })
+      });
     }
   }
-}
+};
 </script>
