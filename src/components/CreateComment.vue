25617c0717b010bf70fd726b0c24f872272a22ce
Vivian Lo <yunyubee@gmail.com>
2019-11-24 17:44:00 +0800

    API integration of CreateComment component

--- src/components/CreateComment.vue
+++ src/components/CreateComment.vue
@@ -2,49 +2,67 @@
   <form @submit.stop.prevent="handleSubmit">
     <div class="form-group">
       <label for="text">留下評論：</label>
-      <textarea
-        v-model="text"
-        class="form-control"
-        rows="3"
-        name="text"
-      />
+      <textarea v-model="text" class="form-control" rows="3" name="text" />
     </div>
     <div class="text-right">
-      <button
-        type="submit"
-        class="btn btn-primary mr-0"
-      >
-        Submit
-      </button>
+      <button type="submit" class="btn btn-primary mr-0" :disabled="isProcessing">Submit</button>
     </div>
   </form>
 </template>
 
 <script>
 /* eslint-disable */
-import uuid from 'uuid/v4'
+// import uuid from "uuid/v4";
+import commentAPI from "./../apis/comments";
+import { Toast } from "./../utils/helpers";
 
 export default {
-  data (){
+  data() {
     return {
-      text : ''
-    }
+      text: "",
+      isProcessing: false
+    };
   },
-  props:{
-    restaurantId:{
+  props: {
+    restaurantId: {
       type: Number,
       required: true
     }
   },
   methods: {
-    handleSubmit (){
-      this.$emit('after-create-comment', {
-        commentId: uuid(),
-        restaurantId: this.restaurantId,
-        text: this.text
-      })
-      this.text = ''
+    async handleSubmit() {
+      try {
+        if (!this.text) {
+          Toast.fire({
+            type: "warning",
+            title: "您尚未填寫任何評論"
+          });
+          return;
+        }
+        this.isProcessing = true;
+
+        const { data, statusText } = await commentAPI.create({
+          restaurantId: this.restaurantId,
+          text: this.text
+        });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        this.$emit("after-create-comment", {
+          commentId: data.commentId,
+          restaurantId: this.restaurantId,
+          text: this.text
+        });
+        this.isProcessing = false;
+        this.text = "";
+      } catch (error) {
+        this.isProcessing = false;
+        Toast.fire({
+          type: "error",
+          title: "無法新增評論，請稍後再試"
+        });
+      }
     }
   }
-}
+};
 </script>
