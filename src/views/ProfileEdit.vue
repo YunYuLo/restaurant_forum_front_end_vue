b4ceea050fc05c0c98f3be603074bc89e8a1fc5c
Vivian Lo <yunyubee@gmail.com>
2019-11-25 14:49:53 +0800

    API integration of UserEdit - get currentUser from vuex and setUser

--- src/views/ProfileEdit.vue
+++ src/views/ProfileEdit.vue
@@ -1,19 +1,113 @@
 <template>
   <div class="container py-5">
-    <ProfileForm @after-submit="handleAfterSubmit" />
+    <form @submit.stop.prevent="handleSubmit">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input
+          id="name"
+          v-model="name"
+          type="text"
+          name="name"
+          class="form-control"
+          placeholder="Enter Name"
+          required
+        />
+      </div>
+
+      <div class="form-group">
+        <label for="image">Image</label>
+        <img v-if="image" :src="image" class="d-block img-thumbnail mb-3" width="200" height="200" />
+
+        <input
+          id="image"
+          type="file"
+          name="image"
+          accept="image/*"
+          class="form-control-file"
+          @change="handleFileChange"
+        />
+      </div>
+
+      <button
+        type="submit"
+        class="btn btn-primary"
+        :disabled="isProcessing"
+      >{{ isProcessing ? "資料更新中..." : "Submit" }}</button>
+    </form>
   </div>
 </template>
 <script>
 /* eslint-disable */
-import ProfileForm from "./../components/ProfileForm";
+import { mapState } from "vuex";
+import usersAPI from "./../apis/users";
+import { Toast } from "./../utils/helpers";
 export default {
-  components: {
-    ProfileForm
+  data() {
+    return {
+      id: 0,
+      name: "",
+      image: "",
+      email: "",
+      isProcessing: false
+    };
+  },
+  computed: {
+    ...mapState(["currentUser"])
+  },
+  watch: {
+    currentUser(user) {
+      this.setUser();
+    }
+  },
+  created() {
+    const { id } = this.$route.params;
+    if (id.toString() !== this.currentUser.id.toString()) {
+      this.$router.push({ name: "not-found" });
+      return;
+    }
+    this.setUser();
   },
   methods: {
-    handleAfterSubmit(formData) {
-      for (let [name, value] of formData.entries()) {
-        console.log(name + ": " + value);
+    setUser() {
+      this.id = this.currentUser.id;
+      this.image = this.currentUser.image;
+      this.name = this.currentUser.name;
+      this.email = this.currentUser.email;
+    },
+    handleFileChange(e) {
+      const files = e.target.files;
+      if (!files.length) return;
+      const imageURL = window.URL.createObjectURL(files[0]);
+      this.image = imageURL;
+    },
+    async handleSubmit(e) {
+      if (!this.name) {
+        Toast.fire({
+          type: "warning",
+          title: "您尚未填寫姓名"
+        });
+        return;
+      }
+      const form = e.target;
+      const formData = new FormData(form);
+      try {
+        this.isProcessing = true;
+        //呼叫 API 更新使用者資料
+        const { data, statusText } = await usersAPI.update({
+          userId: this.id,
+          formData
+        });
+        if (statusText !== "OK" || data.status !== "success") {
+          throw new Error(statusText);
+        }
+        //更新完成後轉址到使用者詳細頁
+        this.$router.push({ name: "user", params: { id: this.id } });
+      } catch (error) {
+        this.isProcessing = false;
+        Toast.fire({
+          type: "error",
+          title: "無法更新使用者資料，請稍後再試"
+        });
       }
     }
   }
